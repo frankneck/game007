a201037939ae32c5b0bfd367b64f5e4acf98cfee
VladIsLoveGo <128213362+VladIsLoveGo@users.noreply.github.com>
2025-03-05 17:57:07 +1000

    add XR camera in FPS

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -7,22 +7,22 @@ using UnityEngine;
 public class PlayerLook : MonoBehaviour
 {
     public Camera cam;
-    [Header("Sensitivity")]
-    public float xSensitivity = 30f;
-    public float ySensitivity = 30f;
-    private float xRotation = 0f;
-    
+    //[Header("Sensitivity")]
+    //public float xSensitivity = 30f;
+    //public float ySensitivity = 30f;
+    //private float xRotation = 0f;
+
     public void ProcessLook(Vector2 input)
     {
-        float mouseX = input.x;
-        float mouseY = input.y;
-        // Calculate camera rotation for looking up and down
-        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
-        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
-        // Use value
-        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        // Rotate player to look right and left
-        transform.Rotate(Vector2.up * (mouseX * Time.deltaTime) * xSensitivity);
+        //float mouseX = input.x;
+        //float mouseY = input.y;
+        //// Calculate camera rotation for looking up and down
+        //xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
+        //xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        //// Use value
+        //cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        //// Rotate player to look right and left
+        //transform.Rotate(Vector2.up * (mouseX * Time.deltaTime) * xSensitivity);
     }
 
 }
