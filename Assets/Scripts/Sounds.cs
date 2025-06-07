7e73f4b79e72238337310969da2eedfdc971d32a
frankneck <finn.f1rst128@gmail.com>
2025-06-07 13:51:38 +1000

    I've made gun sounds and improved 'the reload system'

---
+++ Assets/Scripts/Sounds.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounds : MonoBehaviour
+{
+    public AudioClip[] sounds;
+    private AudioSource audioSrc => GetComponent<AudioSource>();
+
+    public void PlaySound(AudioClip clip, float volume = 1f, bool destroyed = false, float p1 = 0.85f, float p2 = 1.2f)
+    {
+        audioSrc.pitch = Random.Range(p1, p2);
+
+        if (destroyed)
+        {
+            AudioSource.PlayClipAtPoint(clip, transform.position, volume);
+        }
+        else
+        {
+            audioSrc.PlayOneShot(clip, volume);
+        }
+    }
+}
